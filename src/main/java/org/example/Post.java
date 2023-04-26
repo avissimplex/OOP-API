78ab0814dca171dcceadce6e4cdff2644daae62f
Ksenya Sunduova <avissimplex@gmail.com>
2023-04-27 00:29:39 +0400

    Initial commit

---
+++ src/main/java/org/example/Post.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Post {
+    public String name; // поле для имени
+    public String passport; // паспорт
+    public String patronymic; // отчество
+    public String phone; // телефон
+
+    public String surname; // фамилия
+    public boolean subscription; // фамилия
+
+    public FormDate birthday;
+
+}
